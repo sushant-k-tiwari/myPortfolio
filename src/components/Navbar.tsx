bddde5994600aed5e0a1ad4672d18c65b924b326
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-08-28 14:48:29 +0530

    glassmorphism effect added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,49 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-white p-4 px-[144px] flex 1 flex-row justify-between">
-      <div className="text-3xl font-[vm-bold] ">SUSHANT PORTFOLIO</div>
-      <ul className="flex flex-row gap-8 font-[vm-regular]">
-        <li>Home</li>
-        <li>About</li>
-        <li>Experience</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
+    <div className="sticky top-0 z-50 bg-white/30 backdrop-blur-md border-b border-white/20 shadow-sm">
+      <div
+        className=" flex flex-row justify-between p-4 px-[144px]
+      "
+      >
+        <div className="text-3xl font-[vm-bold]">SUSHANT PORTFOLIO</div>
+        <ul className="flex flex-row gap-8 font-[vm-regular]">
+          <li>
+            <a href="#home" className="hover:text-teal-600 transition-colors">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="hover:text-teal-600 transition-colors">
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#experience"
+              className="hover:text-teal-600 transition-colors"
+            >
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              href="#projects"
+              className="hover:text-teal-600 transition-colors"
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className="hover:text-teal-600 transition-colors"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
