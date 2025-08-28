53390e6f39fccb73000051d8de8964b877a104e1
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-08-28 14:48:17 +0530

    added Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,46 @@ const App = () => {
     <div className="relative min-h-screen text-black">
       <Navbar />
       <GridBackground />
-      <div className="relative z-10 p-8">
-        <h1 className="text-4xl font-bold">Welcome!</h1>
-        <p className="mt-4">
-          This is your content on top of the grid background.
-        </p>
+      <div className="relative z-10 p-8 flex flex-col items-center mt-24">
+        <h1 className="text-7xl font-[vm-bold] absolute top-9 text-neutral-600">
+          PORTFOLIO.EXE
+        </h1>
+        <h1 className="text-7xl font-[vm-bold] drop-shadow-2xl">
+          PORTFOLIO.EXE
+        </h1>
+        <h2 className="text-xl font-[vm-medium-italic] text-neutral-400 mt-8">
+          {`> `}React Native App Developer
+        </h2>
+        <div className="w-[50%] ">
+          <p className="text-md font-[vm-regular] p-6">
+            Hi, I'm <span className="p-1 bg-black text-white">Sushant</span>, a
+            passionate and performance-driven App Developer with a strong
+            foundation in Computer Science and Engineering, graduated with a
+            Gold Medal for academic excellence. I specialize in cross-platform
+            mobile development, blending sleek UI design with robust
+            functionality using React Native, Kotlin, and modern technologies.
+          </p>
+        </div>
+        <div className="flex gap-8 font-[vm-regular]">
+          <button className="px-8 py-2 bg-black text-white rounded-md cursor-pointer">
+            Download CV
+          </button>
+          <button className="px-8 py-2 bg-transparent border-neutral-200 border-2 rounded-md cursor-pointer">
+            Download CV
+          </button>
+        </div>
+        <div className="gap-16 flex mt-8">
+          <button className="h-[80px] w-[80px] bg-transparent border-2 border-neutral-200">
+            Github
+          </button>
+          <button className="h-[80px] w-[80px] bg-transparent border-2 border-neutral-200">
+            LinkedIn
+          </button>
+          <button className="h-[80px] w-[80px] bg-transparent border-2 border-neutral-200">
+            Mail
+          </button>
+        </div>
+      <p className="font-[vm-regular] mt-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa veritatis accusamus obcaecati reprehenderit dolorem voluptatem possimus enim in animi quibusdam officiis earum a esse explicabo natus quaerat error eaque cumque, fugiat odio blanditiis voluptas modi consequatur excepturi. Natus alias porro adipisci praesentium earum, soluta ab doloribus quibusdam deserunt repellat aut tempore sit tempora magnam sapiente. Labore doloremque dolorem soluta rerum quae illum est, nemo dignissimos dicta ex aspernatur cum veniam sunt et! Alias corporis beatae eaque error harum. Alias mollitia in, voluptatibus eius quod et amet quam esse tempore nostrum, nulla necessitatibus libero aspernatur odit nisi. Voluptates, quam sit recusandae sapiente accusantium a aperiam cupiditate aliquid praesentium repellendus expedita explicabo voluptate facere! Quam alias quos tempore excepturi, odit assumenda, dicta voluptate ad porro sit aperiam dolor iusto libero pariatur repudiandae esse iste doloremque illum consequuntur autem, facilis eaque omnis eligendi. Similique laudantium iusto tenetur non? Provident consequatur nostrum quas totam magni adipisci non. Tempora deserunt explicabo iusto incidunt perferendis corrupti. Culpa expedita quasi ducimus doloribus magni molestiae voluptatibus error reiciendis asperiores saepe nulla hic, deserunt, ut a repellendus laborum laboriosam modi earum vero sapiente fugiat. Illo eius quibusdam corrupti tenetur, non beatae error ipsam temporibus vitae consectetur! A, quae beatae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui facere reiciendis ea distinctio? Doloremque amet blanditiis, quasi porro molestiae eum placeat laboriosam, quia iusto consequuntur similique impedit inventore cum? Molestiae perspiciatis eum excepturi nihil dicta saepe nostrum reprehenderit sint impedit distinctio! Mollitia, quisquam! Vel placeat asperiores quia deserunt? Rem porro enim cupiditate cumque numquam culpa tempore sit aspernatur labore incidunt quam fugiat id aliquid quasi quas inventore corporis dolor iusto libero voluptatum, est voluptas nihil pariatur. Maiores, dolores eligendi incidunt enim minima, fuga repellendus, saepe explicabo accusamus atque corporis dolore nisi. Quidem quis sequi culpa, ratione itaque cupiditate. Fuga, amet. Magnam id sequi necessitatibus quas asperiores rem tenetur quam dolor et? Nam ullam atque ipsum, dolor suscipit dicta ab eligendi illum, laboriosam incidunt nobis voluptates, quasi obcaecati earum impedit blanditiis dolorem consectetur illo magni a nesciunt. Magnam accusantium cupiditate nemo porro repellendus. Soluta, molestias. Fuga impedit distinctio nemo? Eius, magni excepturi incidunt, fuga amet repellendus id sint saepe minima ducimus, temporibus ea iste! Laboriosam odio quibusdam unde. Quasi non illo in accusamus, earum voluptatum veritatis quos, eaque accusantium quae eum. Neque incidunt sequi nobis non! Ducimus sit porro, sunt molestiae eos, harum dolor nihil ut facere a cumque ex exercitationem, quidem quae nostrum ad voluptatum! Earum nisi, rerum molestiae culpa numquam neque officia, cum eius explicabo cumque iure porro illum non ex aliquam vero doloremque corrupti sit. Ipsa eligendi eos sit molestias deleniti, pariatur repudiandae nam, inventore non nisi quos nobis earum rerum possimus beatae ut rem, maiores ducimus similique maxime? Mollitia, vero. Asperiores autem, sint ex quo maiores beatae facilis nemo aperiam aliquid earum hic fugiat, dicta error. Rem dicta iste voluptas ipsum totam ipsam vero laborum ducimus facilis nesciunt officiis quae modi iure, facere commodi magni repudiandae eum obcaecati nulla perferendis explicabo! Quidem corporis porro harum enim, aliquam ratione quod odit saepe tempora exercitationem. Cum, possimus deserunt. Voluptates, debitis rerum, cupiditate illum dolorem est laborum ipsa eum voluptas repellendus ducimus atque perferendis quisquam cum quidem pariatur natus? Perferendis qui distinctio consequatur vero velit sint illum quaerat ex iure sit aut rem, omnis odit? Itaque corrupti labore quia nulla omnis, placeat sed vel sapiente doloremque architecto tempora minima perspiciatis tenetur rerum illum. Fugit quibusdam vel, aliquam doloremque aspernatur suscipit labore consectetur? Totam repudiandae cupiditate itaque commodi saepe nobis, odit tempora. Eum molestias consequuntur id incidunt neque eligendi perspiciatis fuga deleniti earum, quas optio dignissimos amet atque excepturi eveniet mollitia, officia rerum. Iusto quidem veritatis, nostrum iste dolore ab, asperiores molestias consequuntur nam distinctio amet? Similique iure perspiciatis aliquam minima veritatis. Accusantium vitae quae quaerat expedita nemo laboriosam, ipsum doloremque deserunt commodi illo nihil possimus repudiandae beatae perspiciatis impedit fugit voluptates dolorum voluptatem dolore aut voluptas. Veniam voluptatibus corrupti cum dolore itaque nobis vel, quia soluta iusto, beatae ex error dignissimos odio aspernatur aliquam tempore molestias, dolorem atque sint repudiandae eos quo! Quae labore deserunt perspiciatis totam magnam exercitationem voluptatem hic adipisci repudiandae a neque, voluptates sequi pariatur modi possimus dignissimos voluptas ducimus quod consequuntur molestiae ipsa itaque non quis.</p>
       </div>
     </div>
   );
