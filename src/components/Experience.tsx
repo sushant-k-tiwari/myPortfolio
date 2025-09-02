dbcdf98c8327c5e00c431e8bc6e91461ac86dc3d
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-02 18:56:05 +0530

    Experience section init

---
+++ src/components/Experience.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="relative z-10 p-8 flex flex-col items-center mt-24">
+      <h1 className="text-5xl font-[docade] absolute top-9 text-neutral-600">
+        EXPERIENCE.LOG
+      </h1>
+      <h1 className="text-5xl font-[docade] drop-shadow-2xl">EXPERIENCE.LOG</h1>
+      <div className="w-[80%]">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
