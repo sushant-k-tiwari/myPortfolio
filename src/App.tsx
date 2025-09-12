30266d4eb54351f5046c8348612e370ad80f8489
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-12 09:08:58 +0530

    working links

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,25 @@ const App = () => {
     <div className="relative min-h-screen text-black">
       <Navbar />
       <GridBackground />
-      <HeroSection />
-      <AboutSection />
+      <section id="home">
+        <HeroSection />
+      </section>
+      <section id="about">
+        <AboutSection />
+      </section>
       <TerminalCard>
-        <TerminalCard.Header>~$ CURRENT STATUS:
-
-        <TerminalCard.Header2>I AM ON A JOB HUNT</TerminalCard.Header2>
+        <TerminalCard.Header>
+          ~$ CURRENT STATUS:
+          <TerminalCard.Header2>I AM ON A JOB HUNT</TerminalCard.Header2>
         </TerminalCard.Header>
         <TerminalCard.Body>
           Ready to collaborate on the next big idea!
         </TerminalCard.Body>
       </TerminalCard>
       <Skills />
-      <Experience />
+      <section id="experience">
+        <Experience />
+      </section>
       <div className="text-center text-neutral-500 font-[vm-medium-italic] text-4xl tracking-wide animate-bounce">
         Work in Progress...
       </div>
